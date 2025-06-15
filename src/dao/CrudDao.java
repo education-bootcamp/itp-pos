04bd3b902f0edff401cd2d9a094269c20cb4fbaa
dev-developersstack <education.seekerscloud@gmail.com>
2025-06-15 10:18:35 +0530

    4

--- src/dao/CrudDao.java
+++ src/dao/CrudDao.java
@@ -1,9 +1,9 @@
-package repo;
+package dao;
 
 import java.io.IOException;
 import java.util.List;
 
-public interface CrudRepo <T, ID>{
+public interface CrudDao<T, ID> extends SuperDao{
     public void create(T entity) throws IOException;
     public void update(T entity) throws IOException;
     public void delete(T entity) throws IOException;
